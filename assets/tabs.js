3b0ba539a9ea6f64c74502ded12d1d8d17610434
Ricardo Lopez <rlopez@next-ecommerce.com>
2024-11-28 08:45:20 +0100

    HF Tabs carousel conflict custom-slider

--- assets/tabs.js
+++ assets/tabs.js
@@ -3,10 +3,14 @@ class Tabs extends HTMLElement {
     super();
     this.tabsTitle = this.querySelectorAll('tab-title');
     this.tabsContent = this.querySelectorAll('tab-content');
+    this.tabContentActive = this.querySelectorAll('tab-content[active]');
   }
 
   connectedCallback() {
     console.log('tabs connected');
+    console.log('Tab Active', this.tabContentActive[0]);
+
+    this.initializeSwiper(this.tabContentActive[0]);
   }
 
   removeTitleActive() {
@@ -19,6 +23,60 @@ class Tabs extends HTMLElement {
     this.tabsContent.forEach((tabContent) => {
       tabContent.removeAttribute('active');
     });
+  }
+
+  initializeSwiper(tabContent) {
+    // Busca el contenedor de Swiper dentro del `tabContent` activo
+    const swiperEl = tabContent.querySelector('.collection-block .swiper');
+
+    if (!swiperEl) {
+      console.error('Swiper container not found in the DOM');
+      return;
+    }
+
+    const slides = swiperEl.querySelectorAll('.swiper-slide');
+    const totalSlides = slides.length;
+
+    const swiperParams = {
+      slidesPerView: Math.min(totalSlides, 5),
+      slidesPerGroup: Math.min(totalSlides, 1),
+      loop: false, // Deshabilita el modo loop
+      mousewheel: {
+        forceToAxis: true,
+      },
+      direction: 'horizontal',
+      spaceBetween: 30,
+      navigation: {
+        nextEl: swiperEl.querySelector('.swiper-button-next'),
+        prevEl: swiperEl.querySelector('.swiper-button-prev'),
+      },
+      pagination: false,
+      autoplay: {
+        delay: 5000,
+      },
+      breakpoints: {
+        320: {
+          slidesPerView: Math.min(totalSlides, 1),
+          spaceBetween: 20,
+        },
+        640: {
+          slidesPerView: Math.min(totalSlides, 2),
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: Math.min(totalSlides, 4),
+          spaceBetween: 40,
+        },
+        1024: {
+          slidesPerView: Math.min(totalSlides, 5),
+          spaceBetween: 50,
+        },
+      },
+    };
+
+    // Inicializa Swiper
+
+    new window.Swiper(swiperEl, swiperParams);
   }
 }
 
@@ -40,58 +98,8 @@ class TabTitle extends HTMLElement {
       // this.tabsComponent.tabsContent[index - 1].setAttribute('active', '');
       const tabContent = this.tabsComponent.tabsContent[index - 1];
       tabContent.setAttribute('active', '');
-      this.initializeSwiper(tabContent);
+      this.tabsComponent.initializeSwiper(tabContent);
     });
-  }
-  initializeSwiper(tabContent) {
-    console.log(window.Swiper);
-    // Busca el contenedor de Swiper dentro del `tabContent` activo
-    const swiperEl = tabContent.querySelector('.collection-block .swiper');
-    console.log(swiperEl);
-    if (!swiperEl) {
-      console.error('Swiper container not found in the DOM');
-      return;
-    }
-
-    const swiperParams = {
-      slidesPerView: 5,
-      slidesPerGroup: 1,
-      mousewheel: {
-        forceToAxis: true,
-      },
-      direction: 'horizontal',
-      spaceBetween: 30,
-      grabCursor: true,
-      navigation: {
-        nextEl: swiperEl.querySelector('.swiper-button-next'),
-        prevEl: swiperEl.querySelector('.swiper-button-prev'),
-      },
-      autoplay: {
-        delay: 5000,
-      },
-      breakpoints: {
-        320: {
-          slidesPerView: 1,
-          spaceBetween: 20,
-        },
-        640: {
-          slidesPerView: 2,
-          spaceBetween: 20,
-        },
-        768: {
-          slidesPerView: 4,
-          spaceBetween: 40,
-        },
-        1024: {
-          slidesPerView: 5,
-          spaceBetween: 50,
-        },
-      },
-    };
-
-    // Inicializa Swiper
-
-    new window.Swiper(swiperEl, swiperParams);
   }
 }
 
@@ -100,9 +108,7 @@ class TabContent extends HTMLElement {
     super();
   }
 
-  connectedCallback() {
-    console.log('tab content connected');
-  }
+  connectedCallback() {}
 }
 
 customElements.define('tabs-component', Tabs);
